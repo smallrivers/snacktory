8a8bffedc40e6d1badc044790674a3e631c0747c
Andres Portillo <andres.portillo@gmail.com>
2015-01-28 11:56:41 -0800

    Fix encoding test, update URL

--- src/test/java/de/jetwick/snacktory/HtmlFetcherIntegrationTest.java
+++ src/test/java/de/jetwick/snacktory/HtmlFetcherIntegrationTest.java
@@ -66,8 +66,8 @@ public class HtmlFetcherIntegrationTest {
 //    }
     @Test
     public void testEncoding() throws Exception {
-        JResult res = new HtmlFetcher().fetchAndExtract("http://www.yomiuri.co.jp/science/news/20110415-OYT1T00568.htm", 10000, true);
-        assertEquals("海水汚染には猫トイレの砂…セシウム吸着 : 科学 : YOMIURI ONLINE（読売新聞）", res.getTitle());
+        JResult res = new HtmlFetcher().fetchAndExtract("http://www.yomiuri.co.jp/science/", 10000, true);
+        assertEquals("科学 : 読売新聞（YOMIURI ONLINE）", res.getTitle());
     }
 
     @Test
