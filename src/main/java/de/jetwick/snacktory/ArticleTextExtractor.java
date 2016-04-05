fedd4883fa15d243b843654bc7c646d014c812b6
Andres Portillo <andres.portillo@gmail.com>
2016-04-05 11:07:07 -0700

    Add another extraction case

--- src/main/java/de/jetwick/snacktory/ArticleTextExtractor.java
+++ src/main/java/de/jetwick/snacktory/ArticleTextExtractor.java
@@ -188,7 +188,7 @@ public class ArticleTextExtractor {
                 + "login|si(debar|gn|ngle)");
         setPositive("(^(body|content|h?entry|main|page|post|text|blog|story|haupt))"
                 + "|arti(cle|kel)|instapaper_body|storybody|short-story");
-        setHighlyPositive("news-release-detail|storybody|main-content|articlebody|article_body|articleBody|article-body|html-view-content|entry__body|^main-article$");
+        setHighlyPositive("news-release-detail|storybody|main-content|articlebody|article_body|articleBody|article-body|html-view-content|entry__body|^main-article$|^article__content$");
         setNegative("nav($|igation)|user|com(ment|bx)|(^com-)|contact|"
                 + "foot|masthead|(me(dia|ta))|outbrain|promo|related|scroll|(sho(utbox|pping))|"
                 + "sidebar|sponsor|tags|tool|widget|player|disclaimer|toc|infobox|vcard|post-ratings|title|avatar|follow-me-twitter|truncate");
