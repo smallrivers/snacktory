bba5ca25c4d8a7a7d67d58e47c9eb3945404f6b1
Reynald Borer <reynald.borer@paper.li>
2016-08-12 11:51:00 +0200

    Bump dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -6,9 +6,9 @@ object Dependencies {
   val Slf4jApi = "org.slf4j" % "slf4j-api" % "1.6.6"
   val Slf4jLog4j12 = "org.slf4j" % "slf4j-log4j12" % "1.6.6" 
   val CommonsLang = "commons-lang" % "commons-lang" % "2.6"
-  val CommonsLang3 = "org.apache.commons" % "commons-lang3" % "3.1"
+  val CommonsLang3 = "org.apache.commons" % "commons-lang3" % "3.4"
   val Log4j = "log4j" % "log4j" % "1.2.14"
-  val Guava = "com.google.guava" % "guava" % "18.0"
+  val Guava = "com.google.guava" % "guava" % "19.0"
   val HtmlCleaner = "net.sourceforge.htmlcleaner" % "htmlcleaner" % "2.16"
 }
 
